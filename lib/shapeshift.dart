7f6cc93d1f08824dd4411f51ec3daef629a488b3
Sam Rawlins <srawlins@google.com>
2014-10-16 19:19:02 -0700

    Separating out json_diff from shapeshift

--- lib/shapeshift.dart
+++ lib/shapeshift.dart
@@ -3,11 +3,9 @@ library shapeshift;
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'package:collection/equality.dart';
 import 'package:diff_match_patch/diff_match_patch.dart';
+import 'package:json_diff/json_diff.dart';
 
-part 'src/diff_node.dart';
-part 'src/json_differ.dart';
 part 'src/package_reporter.dart';
 part 'src/markdown_writer.dart';
 part 'src/dartdoc_viewer_utils.dart';
