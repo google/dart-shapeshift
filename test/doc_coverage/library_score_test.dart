85f800d577783623716abb460c8c13c6b3214f63
Sam Rawlins <srawlins@google.com>
2015-03-05 10:33:22 -0800

    Fix up my library score tests; still a lot of TODOs

--- test/doc_coverage/library_score_test.dart
+++ test/doc_coverage/library_score_test.dart
@@ -8,7 +8,7 @@ import 'dart:convert';
 import 'package:shapeshift/doc_coverage_common.dart';
 import 'package:unittest/unittest.dart';
 
-Map<String,dynamic> libraryTemplate = {
+Map<String,dynamic> get libraryTemplate => {
   "name": "foo",
   "qualifiedName": "foo",
   "comment": "",
@@ -55,100 +55,111 @@ List<Map> functionScores(Map lib) {
   return scores;
 }
 
+List<Map> variableScores(Map lib) {
+  List<Map> scores = new List();
+    scores.addAll(lib['variables'].values.map((Map variable) {
+        return {
+          'size': 1,
+          'score': DocCoverage.scoreThing(variable)
+        };
+    }));
+  return scores;
+}
+
+List<Map> allScores(Map lib) {
+  return new List()
+    ..addAll(classScores(lib))
+    ..addAll(functionScores(lib))
+    ..addAll(variableScores(lib));
+}
+
 void main() {
-  // TODO: THIS
-  test('scores a library without a comment, and without classes or functions', () {
+  // TODO: This.
+  test('scores a library without a comment, and without classes or members', () {
     Map<String,dynamic> lib = new Map.from(libraryTemplate);
     
     List<Map> scores = new List();
-    scores.addAll(classScores(lib));
-    scores.addAll(functionScores(lib));
+    scores.addAll(allScores(lib));
     double score = DocCoverage.weightedScore(scores);
     expect(score, equals(1.0));
   });
 
-  // TODO: THIS
-  test('scores a library with a brief comment, and without classes or functions', () {
+  // TODO: This.
+  test('scores a library with a brief comment, and without classes or members', () {
     Map<String,dynamic> lib = new Map.from(libraryTemplate)
       ..['comment'] = 'brief';
-    
-    List<Map> scores = new List();
-    scores.addAll(classScores(lib));
-    scores.addAll(functionScores(lib));
-    double score = DocCoverage.weightedScore(scores);
+
+    double score = DocCoverage.weightedScore(allScores(lib));
     expect(score, equals(1.0));
   });
 
-  test('scores a library with a good comment, and without classes or functions', () {
+  // TODO: This.
+  test('scores a library with a good comment, and without classes or members', () {
     Map<String,dynamic> lib = new Map.from(libraryTemplate)
       ..['comment'] = 'A comment\n\nwith a nice and detailed paragraph.';
-    
-    List<Map> scores = new List();
-    scores.addAll(classScores(lib));
-    scores.addAll(functionScores(lib));
-    double score = DocCoverage.weightedScore(scores);
+
+    double score = DocCoverage.weightedScore(allScores(lib));
     expect(score, equals(1.0));
   });
 
   test('1 undocumented function', () {
-    Map<String,dynamic> lib = newLibraryWithFunctions(
-        'A comment\n\nwith a nice and detailed paragraph.', ['']);
-    
-    List<Map> scores = new List();
-    scores.addAll(classScores(lib));
-    scores.addAll(functionScores(lib));
-    double score = DocCoverage.weightedScore(scores);
+    Map<String,dynamic> lib = newLibraryWith(
+        'A comment\n\nwith a nice and detailed paragraph.', funcComments: ['']);
+
+    double score = DocCoverage.weightedScore(allScores(lib));
     expect(score, equals(0.0));
   });
 
-  test('1 undocumented function', () {
-    Map<String,dynamic> lib = newLibraryWithFunctions(
-        'A comment\n\nwith a nice and detailed paragraph.', ['']);
-    
-    List<Map> scores = new List();
-    scores.addAll(classScores(lib));
-    scores.addAll(functionScores(lib));
-    double score = DocCoverage.weightedScore(scores);
-    expect(score, equals(0.0));
-  });
-
-  test('1 method doc with long summary', () {
-    Map<String,dynamic> lib = newLibraryWithFunctions(
+  test('1 function doc with long summary', () {
+    Map<String,dynamic> lib = newLibraryWith(
         'A comment\n\nwith a nice and detailed paragraph.',
-        ['This first line is the summary and should be short but its so ' +
+        funcComments: ['This first line is the summary and should be short but its so ' +
          'long wow no one could ever think that this summarizes the ' +
          'behavior of a method.']);
-    
-    List<Map> scores = new List();
-    scores.addAll(classScores(lib));
-    scores.addAll(functionScores(lib));
-    double score = DocCoverage.weightedScore(scores);
+
+    double score = DocCoverage.weightedScore(allScores(lib));
     expect(score, equals(0.8));
   });
 
-  test('1 method with docs w/o period', () {
-    Map<String,dynamic> lib = newLibraryWithFunctions(
+  test('1 function with docs w/o period', () {
+    Map<String,dynamic> lib = newLibraryWith(
         'A comment\n\nwith a nice and detailed paragraph.',
-        ['Where\'s the period']);
-    
-    List<Map> scores = new List();
-    scores.addAll(classScores(lib));
-    scores.addAll(functionScores(lib));
-    double score = DocCoverage.weightedScore(scores);
+        funcComments: ['Where\'s the period']);
+
+    double score = DocCoverage.weightedScore(allScores(lib));
     expect(score, equals(0.9));
+  });
+
+  test('1 undocumented variable', () {
+    Map<String,dynamic> lib = newLibraryWith(
+        'A comment\n\nwith a nice and detailed paragraph.', varComments: ['']);
+
+    double score = DocCoverage.weightedScore(allScores(lib));
+    expect(score, equals(0.0));
   });
 }
 
-Map<String,dynamic> newLibraryWithFunctions(String libComment, List<String> comments) {
+Map<String,dynamic> newLibraryWith(String libComment,
+    { List<String> funcComments: const [], List<String> varComments: const [] }) {
+  Map<String,dynamic> lib = libraryTemplate
+    ..['comment'] = libComment;
   int counter = 0;
-  Map<String,dynamic> lib = new Map.from(libraryTemplate)
-      ..['comment'] = libComment;
-  comments.forEach((String c) {
+
+  funcComments.forEach((String c) {
     counter++;
     lib['functions']['methods']['m$counter'] = {
-        "name": "m$counter",
-        "qualifiedName": "foo.Foo.m$counter",
-        "comment": c
+        'name': 'm$counter',
+        'qualifiedName': 'foo.Foo.m$counter',
+        'comment': c
+    };
+  });
+
+  varComments.forEach((String c) {
+    counter++;
+    lib['variables']['v$counter'] = {
+        'name': 'v$counter',
+        'qualifiedName': 'foo.Foo.v$counter',
+        'comment': c
     };
   });
 
